e6921d832991da2bba38da95bc97b331c34b6e4d
Squareben1 <bengittins@live.com>
2020-04-19 15:02:56 +0100

    refactored rack empty and full checks to their own methods

--- lib/docking_station.py
+++ lib/docking_station.py
@@ -7,16 +7,26 @@ class DockingStation:
         self.rack = []
         self.max_capacity = 20
 
+    def rack_full(self):
+        if len(self.rack) >= self.max_capacity:
+            return True
+        else:
+            return False
+    def rack_empty(self):
+        if len(self.rack) > 0:
+            return False
+        else:
+            return True
 
     def deposit(self, bike):
-        if len(self.rack) >= self.max_capacity:
+        if self.rack_full():
             raise ValueError('Rack is full')
         else:
             self.rack.append(bike)
             return self.rack
 
     def release(self):
-        if len(self.rack) > 0:
+        if not self.rack_empty():
             return self.rack.pop()
         else:
             raise ValueError('Rack is empty')
